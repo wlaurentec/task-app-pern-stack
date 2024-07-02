36d4b3b85258244c1a9d1ea987a0bb733b830cc6
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-02 11:16:23 -0500

    add enviroment variables and config cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const tasksRoutes = require("./routes/tasks.routes");
 
 const app = express();
+
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(tasksRoutes);
