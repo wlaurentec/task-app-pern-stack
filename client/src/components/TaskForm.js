b7b2b55f03f994c87427d1ee204ee0ac9f858456
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-02 17:53:48 -0500

    add task-form

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,11 +1,72 @@
-import React from 'react'
-
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState, useEffect } from "react";
 function TaskForm() {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(task);
+    event.target.reset();
+  };
+
+  const handleChange = (event) => {
+    setTask({ ...task, [event.target.name]: event.target.value });
+  };
   return (
-    <div>
-      TaskForm
-    </div>
-  )
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={3}>
+        <Card
+          sx={{ mt: 5 }}
+          style={{ backgroundColor: "#1e2727", padding: "1rem" }}
+        >
+          <Typography variant="5" textAlign="center" color="white">
+            Create Task
+          </Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                variant="filled"
+                label="Write your title"
+                sx={{ display: "block", margin: "0.5rem 0" }}
+                name="title"
+                onChange={handleChange}
+                inputProps={{ style: { color: "white" } }}
+                InputLabelProps={{ style: { color: "white" } }}
+              />
+              <TextField
+                variant="filled"
+                label="Write your description"
+                multiline
+                rows={4}
+                name="description"
+                onChange={handleChange}
+                sx={{ display: "block", margin: "0.5rem 0" }}
+                inputProps={{ style: { color: "white" } }}
+                InputLabelProps={{ style: { color: "white" } }}
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Save
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default TaskForm
+export default TaskForm;
